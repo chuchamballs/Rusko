73358023a93f26a374eb36c27cf8d83098444545
vaclav.michalek <vaclav.michalek@VT1-05>
2022-02-28 13:25:13 +0100

    Přidejte soubory projektu.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hackovaní Ruska");
+for(int i = 0; i <= 100; i = i+20)
+{
+    Console.WriteLine("Postup " + i + "%");
+    Thread.Sleep(1000);
+}
+Console.WriteLine("Rusko úspěšně hacknuto :)");
